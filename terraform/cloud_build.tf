c9d23784d7ef85de249a3ddf2f04222427bea512
Masafumi Higashi <higashi-masafumi507@g.ecc.u-tokyo.ac.jp>
2025-07-17 14:28:15 +0900

    feat: make texlive base image for backend

--- terraform/cloud_build.tf
+++ terraform/cloud_build.tf
@@ -28,4 +28,31 @@ resource "google_cloudbuild_trigger" "main_branch" {
   depends_on = [google_project_service.required_apis]
 }
 
+resource "google_cloudbuild_trigger" "manual_base_image_build" {
+  name            = "manual-build-backend-base-image"
+  description     = "Manually trigger to build the backend base image with TeX Live."
+  service_account = google_service_account.cloud_build.id
+  filename        = "cloudbuild-base.yaml"
+
+  # Same GitHub configuration as main_branch trigger
+  github {
+    owner = "Higashi-Masafumi"  # Same as main_branch trigger
+    name  = "jaXiv"             # Same as main_branch trigger
+
+    pull_request {
+      branch = "^main$"
+    }
+  }
+
+  substitutions = {
+    _REGION                = var.region
+    _REPOSITORY            = google_artifact_registry_repository.main.name
+    _BACKEND_SERVICE_NAME  = google_cloud_run_v2_service.backend.name
+    _FRONTEND_SERVICE_NAME = google_cloud_run_v2_service.frontend.name
+    _CLOUD_RUN_SA          = google_service_account.cloud_run.email
+  }
+
+  depends_on = [google_project_service.required_apis]
+}
+
 # Output moved to outputs.tf 
